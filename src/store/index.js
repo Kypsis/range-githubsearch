2279b999d172555ffe47336c51176c1af1b47f60
Harry Sild <puhhbaer@hotmail.com>
2020-02-24 19:13:08 +0200

    search functionality

--- src/store/index.js
+++ src/store/index.js
@@ -6,24 +6,34 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    results: []
+    results: [],
+    loading: false
   },
   mutations: {
     fetchData(state, payload) {
       state.results = payload;
+    },
+    setLoading(state) {
+      state.loading = !state.loading;
     }
   },
   actions: {
+    setLoading(context) {
+      context.commit("setLoading");
+    },
     fetchData(context, query = "web") {
+      context.dispatch("setLoading");
       axios
         .get(`https://api.github.com/search/repositories?q=${query}`)
         .then(response => {
           context.commit("fetchData", response.data.items);
         })
+        .then(() => context.dispatch("setLoading"))
         .catch(error => {
           console.log(error.message);
         });
     }
   },
+
   modules: {}
 });
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="search">
     <h1>Search</h1>
-    <div id="example-1">
+    <div>
+      <form @submit.prevent="submitSearch">
+        <input
+          v-model="searchTerm"
+          @submit="submitSearch"
+          placeholder="Search for repo"
+        />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+
+    <div id="results-loading" v-if="loading">
+      <div>Loading...</div>
+    </div>
+    <div id="results-list" v-if="!loading">
       <div v-for="result in results" :key="result.id">
         {{ result.full_name }}
       </div>
@@ -21,13 +35,23 @@ export default {
   created: function() {
     this.$store.dispatch("fetchData", "studio");
   },
-  data: () => ({}),
+  data: () => ({
+    searchTerm: ""
+  }),
   computed: {
     results: function() {
       return this.$store.state.results;
+    },
+    loading: function() {
+      return this.$store.state.loading;
     }
   },
   watch: {},
-  methods: {}
+  methods: {
+    submitSearch: function() {
+      this.$store.dispatch("fetchData", this.searchTerm);
+      this.searchTerm = "";
+    }
+  }
 };
 </script>
